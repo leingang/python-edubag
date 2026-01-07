f62a6d6a415e62a9096af5701543271ba6fb05bc
Matthew Leingang <leingang@nyu.edu>
2026-01-07 12:43:58 -0500

    Install script entry point

--- src/edubag/__main__.py
+++ src/edubag/__main__.py
@@ -1,4 +1,6 @@
-from .cli import app
+"""Entry point for the edubag CLI."""
+# Import the main app module which will register all subcommands
+import edubag
 
 if __name__ == "__main__":
-    app()
+    edubag.app()
